1dce3b54320e53d0cddccbacf1419768f01d491f
Ayden Scott <aydenscott444@gmail.com>
2024-02-27 16:35:45 +0000

    Added routes for pages

--- app.rb
+++ app.rb
@@ -4,3 +4,20 @@ require "sinatra/reloader"
 get("/") do
   erb(:homepage)
 end
+
+get('/rock') do
+  moves = [rock,paper,scissors]
+  comp_move =moves.sample 
+  "<h2>We played rock<h2>
+  
+  <h2>They played #{comp_move}</h2>
+  
+  "
+end
+
+get('/paper') do
+  erb(:paper)
+end
+get('/scissors') do
+  erb(:scissors)
+end
