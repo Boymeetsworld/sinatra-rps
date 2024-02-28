725361ca9a129085c96d4dbaf9969de81a998e18
Ayden Scott <aydenscott444@gmail.com>
2024-02-28 15:48:06 +0000

    Updated app now up and running <3

--- app.rb
+++ app.rb
@@ -6,18 +6,82 @@ get("/") do
 end
 
 get('/rock') do
-  moves = [rock,paper,scissors]
+  moves = ['rock','paper','scissors']
   comp_move =moves.sample 
-  "<h2>We played rock<h2>
-  
-  <h2>They played #{comp_move}</h2>
-  
-  "
+
+  if comp_move == 'rock'
+    outcome = 'tied'
+
+  elsif comp_move == 'paper'
+    outcome = 'lost'
+
+    elsif comp_move == 'scissors'
+      outcome = 'won'
+  end
+"
+<a href= /rock>Play Rock</a>
+<a href= /paper>Play Paper</a>
+<a href= /scissors>Play Scissors</a>
+
+<h2>We played rock<h2>
+
+<h2>They played #{comp_move} </h2>
+
+<h2>We #{outcome} </h2>
+<a href= />Rules</a>
+"
 end
 
 get('/paper') do
-  erb(:paper)
+  moves = ['rock','paper','scissors']
+  comp_move =moves.sample 
+
+  if comp_move == 'paper'
+    outcome = 'tied'
+
+  elsif comp_move == 'scissors'
+    outcome = 'lost'
+
+    elsif comp_move == 'rock'
+      outcome = 'won'
+  end
+  "
+<a href= /rock>Play Rock</a>
+<a href= /paper>Play Paper</a>
+<a href= /scissors>Play Scissors</a>
+
+<h2>We played paper<h2>
+
+<h2>They played #{comp_move} </h2>
+
+<h2>We #{outcome} </h2>
+<a href= />Rules</a>
+  "
 end
 get('/scissors') do
-  erb(:scissors)
+
+  moves = ['rock','paper','scissors']
+  comp_move =moves.sample 
+
+  if comp_move == 'paper'
+    outcome = 'tied'
+
+  elsif comp_move == 'scissors'
+    outcome = 'lost'
+
+    elsif comp_move == 'rock'
+      outcome = 'won'
+  end
+  "
+<a href= /rock>Play Rock</a>
+<a href= /paper>Play Paper</a>
+<a href= /scissors>Play Scissors</a>
+
+<h2>We played scissors<h2>
+
+<h2>They played #{comp_move} </h2>
+
+<h2>We #{outcome} </h2>
+<a href= />Rules</a>
+  "
 end
